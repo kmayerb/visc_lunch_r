43d0e2e172140b1ae0e6b01f1641ce4e63ffdd43
kmayerb <kmayerb@gmail.com>
2025-09-22 11:51:47 -0700

    Moved files to subdirectory

--- my-r-package/R/hello.R
+++
@@ -1,4 +0,0 @@
-# File: /my-r-package/my-r-package/R/hello.R
-hello <- function() {
-  return("Hello, world!")
-}
--- my-r-package/tests/testthat/test-hello.R
+++
@@ -1,5 +0,0 @@
-library(testthat)
-
-test_that("hello function returns correct greeting", {
-  expect_equal(hello(), "Hello, world!")
-})
